374b74cb01e6aa79c63cf5c5e491818fe81253ab
Anxhelo <anxhelo_shperdheja@hotmail.com>
2025-08-12 10:48:40 +0200

    enable plain text

--- src/KafkaConnector.php
+++ src/KafkaConnector.php
@@ -14,11 +14,18 @@ class KafkaConnector implements ConnectorInterface
 
         $conf->set('bootstrap.servers', $config['bootstrap_servers']);
         $conf->set('security.protocol', $config['security_protocol']);
-        $conf->set('sasl.mechanisms', $config['sasl_mechanisms']);
-        $conf->set('sasl.username', $config['sasl_username']);
-        $conf->set('sasl.password', $config['sasl_password']);
 
-        $producer = new \RdKafka\Producer($conf);
+	    if (!empty($config['sasl_mechanisms'])) {
+		    $conf->set('sasl.mechanisms', $config['sasl_mechanisms']);
+	    }
+	    if (!empty($config['sasl_username'])) {
+		    $conf->set('sasl.username', $config['sasl_username']);
+	    }
+	    if (!empty($config['sasl_password'])) {
+		    $conf->set('sasl.password', $config['sasl_password']);
+	    }
+
+	    $producer = new \RdKafka\Producer($conf);
 
         $conf->set('group.id', $config['group_id']);
         $conf->set('auto.offset.reset', 'earliest');
